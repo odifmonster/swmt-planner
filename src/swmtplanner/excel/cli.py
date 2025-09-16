9d476b2aeb1b59cc5ffd3fcc0f7daa5f9642a2f2
odifmonster <lamanwyner@gmail.com>
2025-09-15 21:39:09 -0400

    version 0.0.4, added a gen-info subcommand and started on the README

--- src/swmtplanner/excel/cli.py
+++ src/swmtplanner/excel/cli.py
@@ -2,11 +2,14 @@
 
 import typer
 
-from .excel import update_file, generate_report
+from .excel import update_file, generate_report, gen_info_template
 
 app = typer.Typer()
-app.command('update', no_args_is_help=True)(update_file)
-app.command('report', no_args_is_help=True)(generate_report)
+app.command('update', no_args_is_help=True,
+            help='Update a product data file.')(update_file)
+app.command('report', no_args_is_help=True,
+            help='Generate a named report to an output file.')(generate_report)
+app.command('gen-info', help='Generate an info file template.')(gen_info_template)
 
 if __name__ == '__main__':
     app()
--- src/swmtplanner/excel/excel.py
+++ src/swmtplanner/excel/excel.py
@@ -7,6 +7,111 @@ from enum import Enum
 
 from .info import INFO_MAP, load_info_map
 
+_INFO_OUT_HELP = 'Path to the new file to generate'
+_InfoOutAnno = Annotated[Path, typer.Option(help=_INFO_OUT_HELP,
+                                            writable=True, dir_okay=False)]
+
+def gen_info_template(outpath: _InfoOutAnno = 'excel-info.txt'):
+    outfile = open(outpath, mode='w+')
+
+    outfile.write('# EDIT DATA SOURCE INFO HERE\n\n')
+
+    outfile.write('# Any text following a pound sign is ignored\n')
+    outfile.write('# End a line in ... if you want the next line to be included')
+    outfile.write(' as part of the previous one\n')
+    outfile.write('# Names with spaces and special characters must be')
+    outfile.write(' wrapped in quotes\n')
+    outfile.write('# All file paths must be wrapped in quotes\n\n')
+
+    outfile.write('# Values assigned outside of info blocks can be')
+    outfile.write(' re-used elsewhere, with a preceding\n')
+    outfile.write('# \'$\' to mark it as a reference and not a regular name\n\n')
+
+    outfile.write('FOLDER = <PATH TO MAIN FOLDER HERE>\n')
+    outfile.write('WORKBOOK = <PATH TO MAIN WORKBOOK HERE>\n\n')
+
+    outfile.write('# any values that should be interpreted as a list must')
+    outfile.write(' be preceded by a *\n')
+    outfile.write('*jets = "JET 1", "JET 2", "JET 3", "JET 4", "JET 7", "JET 8",')
+    outfile.write(' "JET 9", "JET 10"\n')
+    outfile.write('*req_wks = "FAB req\'d WK0", "FAB req\'d WK1", "FAB req\'d WK2",')
+    outfile.write(' "FAB req\'d WK3", ...\n')
+    outfile.write('    "FAB req\'d WK4", "FAB req\'d WK5"\n\n')
+
+    outfile.write('dye_formulae:\n')
+    outfile.write('    # This is always required, must be an absolute path to a file\n')
+    outfile.write('    folder=$FOLDER\n    workbook=LamDemandPlanning.xlsx\n')
+    outfile.write('    sheet=Xref # This is always required\n')
+    outfile.write('    col_names="COLOR NAME", "COLOR NUMBER", "SHADE RATING"\n\n')
+
+    outfile.write('# information about fabric items (which jets they can run on,\n')
+    outfile.write('# which greige style they use, etc.)\n')
+    outfile.write('pa_fin_items:\n    folder=$FOLDER\n')
+    outfile.write('    workbook=LamDemandPlanning.xlsx\n    sheet=Xref\n')
+    outfile.write('    col_names="GREIGE ITEM", STYLE, WD, Yield, STATUS,')
+    outfile.write(' "COLOR NAME", "COLOR NUMBER", ...\n')
+    outfile.write('        "PA FIN ITEM", $jets, "SHADE RATING"\n\n')
+
+    outfile.write('greige_styles: # information about target lbs per greige style roll\n')
+    outfile.write('    folder=$FOLDER\n    workbook=LamDemandPlanning.xlsx\n')
+    outfile.write('    sheet="Griege Sizes"\n\n')
+
+    outfile.write('greige_translation:\n    folder=$FOLDER\n')
+    outfile.write('    workbook=LamDemandPlanning.xlsx\n')
+    outfile.write('    sheet="Greige Style Translation"\n')
+    outfile.write('    subst_names=inventory, plan ')
+    outfile.write('# only use this if the workbook has no column names\n')
+    outfile.write('    col_ranges="A:B" # required if providing column names\n')
+    outfile.write('    start_row=2 # the first row of data to start reading\n\n')
+
+    outfile.write('jet_info:\n    folder=$FOLDER\n    workbook=master.xlsx\n')
+    outfile.write('    sheet=jets\n\n')
+
+    outfile.write('pa_inventory:\n    folder=$FOLDER\n    workbook=$WORKBOOK\n')
+    outfile.write('    sheet=1427\n')
+    outfile.write('    col_names=Roll, Item, Quality, Pounds, ASSIGNED_ORDER\n')
+    outfile.write('    start_row=4\n\n')
+
+    outfile.write('pa_floor_mos:\n    folder=$FOLDER\n')
+    outfile.write('    workbook=$WORKBOOK\n    sheet=1427\n')
+    outfile.write('    col_names="Item\\nType", Warehouse, Customer, Owner, ')
+    outfile.write('Roll, Lot, Item, ...\n')
+    outfile.write('        Quality, "Nominal\\nWidth", Quantity, DEFECT1, ')
+    outfile.write('DEF1_REASON, DEFECT2, ...\n')
+    outfile.write('        DEF2_REASON, DEFECT3, DEF3_REASON, MARKET_SEGMENT\n')
+    outfile.write('    start_row=4\n\n')
+
+    outfile.write('adaptive_orders:\n    folder=$FOLDER\n')
+    outfile.write('    workbook=$WORKBOOK\n    sheet=Adaptive\n')
+    outfile.write('    col_names=Machine, StartTime, EndTime, FinItem, ')
+    outfile.write('DyelotID1, DyelotID2, Width, Qty\n\n')
+
+    outfile.write('si_release:\n    folder=$FOLDER\n')
+    outfile.write('    workbook="SI release 20250825.xlsx"\n')
+    outfile.write('    sheet="Vendor Release Report - Weekly "\n')
+    outfile.write('    col_ranges="F,AS:AT"\n')
+    outfile.write('    subst_names=greige, daily_lbs, weekly_lbs\n')
+    outfile.write('    start_row=3\n    end_row=25\n\n')
+
+    outfile.write('wf_release:\n    folder=$FOLDER\n')
+    outfile.write('    workbook="SI release 20250825.xlsx"\n')
+    outfile.write('    sheet="Vendor Release Report - Weekly "\n')
+    outfile.write('    col_ranges="F,AU"\n    subst_names=greige, weekly_lbs\n')
+    outfile.write('    start_row=29\n    end_row=35\n\n')
+
+    outfile.write('lam_ship_dates:\n    folder=$FOLDER\n')
+    outfile.write('    workbook=$WORKBOOK\n    sheet="Lam Release"\n')
+    outfile.write('    col_names="Stock Item", "Ply1 Item", "Ship Day"\n\n')
+
+    outfile.write('pa_reqs:\n    folder=$FOLDER\n')
+    outfile.write('    workbook=$WORKBOOK\n    sheet="Fab shortage 9-15"\n')
+    outfile.write('    col_names="PA Item", "Ply1 Item", "FAB req\'d past due",')
+    outfile.write(' $req_wks, "PA Fin", ...\n')
+    outfile.write('        "Inspection", "Frame", "Dye Orders"')
+
+    outfile.truncate()
+    outfile.close()
+
 def df_cols_as_str(df: pd.DataFrame, *args):
     for colname in args:
         df[colname] = df[colname].astype('string')
