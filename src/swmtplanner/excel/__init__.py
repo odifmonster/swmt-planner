5c3912bedc882df4c0b4990c7cd477b13b4c8c59
odifmonster <lamanwyner@gmail.com>
2025-09-17 11:59:16 -0400

    removed all mentions of excel module

--- src/swmtplanner/excel/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from . import interpreter, cli
-from .info import INFO_MAP, info_to_pdargs, load_info_map
-from .excel import df_cols_as_str
-
-__all__ = ['interpreter', 'df_cols_as_str', 'cli', 'INFO_MAP', 'info_to_pdargs',
-           'load_info_map']
--- src/swmtplanner/excel/interpreter/file/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from .file import FilePos, CharStream
-
-__all__ = ['FilePos', 'CharStream']
