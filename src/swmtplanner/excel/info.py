4760fe2db7cc38ffe4697d4ab7153ff36eeab35a
odifmonster <lamanwyner@gmail.com>
2025-12-10 09:55:20 -0500

    another unhelpful commit message

--- src/swmtplanner/excel/info.py
+++ src/swmtplanner/excel/info.py
@@ -8,9 +8,11 @@ _INFO = {}
 
 def _check_required_keys(name: str, block: dict):
     req_map = {
+        'Buildable': { 'source', 'dest' },
         'Excel': { 'folder', 'workbook', 'sheet' },
         'CSV': { 'folder', 'file' },
         'JSON': { 'folder', 'file' },
+        'DatFile': { 'folder', 'file', 'columns' },
         'ProductInfo': { 'dye_formulae', 'fab_items', 'greige_styles' },
         'DyeOrders': { 'adaptive', 'pa_714', 'fab_items' },
         'PADyePlan': { 'future_orders', 'avail_greige', 'safety_tgts' },
@@ -28,7 +30,7 @@ def df_cols_as_str(df: pd.DataFrame, cols: list[str]):
         df[col] = df[col].astype('string')
     return df
 
-def _load_excel(name: str, block: dict):
+def _load_excel(name: str, block: dict, rebuild: bool = False):
     fpath = os.path.join(block['folder'].data, block['workbook'].data)
     pdargs = {
         'sheet_name': block['sheet'].data, 'header': 0
@@ -65,31 +67,43 @@ def _load_excel(name: str, block: dict):
     if 'col_ranges' in block and 'usecols' not in pdargs:
         pdargs['usecols'] = block['col_ranges'].data
     
+    if 'str_cols' in block:
+        pdargs['dtype'] = { x.data: 'string' for x in block['str_cols'] }
+    
     df = pd.read_excel(fpath, **pdargs)
     return df
 
-def _load_csv(name: str, block: dict):
+def _load_csv(name: str, block: dict, rebuild: bool = False):
     fpath = os.path.join(block['folder'].data, block['file'].data)
     return pd.read_csv(fpath)
 
-def _load_json(name: str, block: dict):
+def _load_json(name: str, block: dict, rebuild: bool = False):
     fpath = os.path.join(block['folder'].data, block['file'].data)
     src = open(fpath)
     res = json.loads(src.read())
     src.close()
     return res
 
-def _series_first(srs: pd.Series):
-    return list(srs)[0]
+def _load_dat_file(name: str, block: dict, rebuild: bool = False):
+    fpath = os.path.join(block['folder'].data, block['file'].data)
+    return pd.read_csv(fpath, sep='\t', header=None, names=list(map(lambda x: x.data, block['columns'])))
+
+def _get_jets(row):
+    jets = []
+    for i in filter(lambda i: i < 4 or i > 5, range(10)):
+        if not pd.isna(row[f'JET {i+1}']):
+            jets.append(f'Jet-{i+1:02}')
+    return ','.join(jets)
 
 def _convert_fab_items(fab_df: pd.DataFrame):
+    is_cat = fab_df['GREIGE ITEM'].str.contains('CAT')
     no_fin = fab_df['PA FIN ITEM'].isna()
     no_yld = fab_df['Yield'].isna()
     no_color = fab_df['COLOR NUMBER'].isna()
     no_shade = fab_df['SHADE RATING'].isna()
     status_ok = (fab_df['STATUS'] == 'A') | fab_df['STATUS'].isna()
 
-    drop_rows = fab_df[no_fin | no_yld | no_color | no_shade | ~status_ok].index
+    drop_rows = fab_df[is_cat | no_fin | no_yld | no_color | no_shade | ~status_ok].index
     fab_df = fab_df.drop(drop_rows)
 
     fab_df.loc[:, 'GREIGE ITEM'] = fab_df['GREIGE ITEM'].str.upper().apply(lambda s: s.strip())
@@ -100,134 +114,114 @@ def _convert_fab_items(fab_df: pd.DataFrame):
     
     return fab_df
 
-def _load_product_info(name: str, block: dict):
-    dye_df = _load_block('dye_formulae', block['dye_formulae'])
-    fab_df = _load_block('fab_items', block['fab_items'])
-    grg_df = _load_block('greige_styles', block=['greige_styles'])
+def _load_buildable(name: str, block: dict, rebuild: bool = False):
+    _check_required_keys('dest', block['dest'])
 
-    dye_df = dye_df[~(dye_df['COLOR NUMBER'].isna() | dye_df['SHADE RATING'].isna())]
-    dye_df['COLOR NUMBER'] = dye_df['COLOR NUMBER'].astype('int64')
-    unique_dyes = dye_df.groupby('COLOR NUMBER').agg(
-        name=pd.NamedAgg(column='COLOR NAME', aggfunc=_series_first),
-        shade=pd.NamedAgg(column='SHADE RATING', aggfunc=_series_first)
-    )
-    unique_dyes = unique_dyes.reset_index()
+    if rebuild:
+        res = _load_block('source', block['source'], rebuild=rebuild)
 
-    fab_df = _convert_fab_items(fab_df)
-    fab_df = fab_df.merge(grg_df, how='left', left_on='GREIGE ITEM', right_on='GreigeAlt')
-    fab_df = fab_df[~fab_df['GREIGE ITEM'].isna()]
+        match name:
+            case 'greige_translation':
+                res['inventory'] = res['inventory'].str.upper()
+                res['plan'] = res['plan'].str.upper()
+                to_drop = res[res['plan'].str.contains('DEV|USED')].index
+                res = res.drop(to_drop)
+            case 'greige_styles':
+                to_drop = res[res['WeeklyLbs'] == 0].index
+                res = res.drop(to_drop)
+            case 'dye_formulae':
+                to_drop = res[res['COLOR NUMBER'].isna() | res['SHADE RATING'].isna()].index
+                res = res.drop(to_drop)
+                res['COLOR NUMBER'] = res['COLOR NUMBER'].astype('int64')
+                unique = res.groupby('COLOR NUMBER').agg(
+                    name=pd.NamedAgg(column='COLOR NAME', aggfunc=_series_first),
+                    shade=pd.NamedAgg(column='SHADE RATING', aggfunc=_series_first)
+                )
+                res = unique.reset_index()
+            case 'fab_items':
+                res = _convert_fab_items(res)
+                res['jets'] = res.agg(_get_jets, axis=1)
+                unique = res.groupby('PLY 1 PART #').agg(
+                    pa_item=pd.NamedAgg(column='item', aggfunc=_series_first),
+                    master=pd.NamedAgg(column='STYLE', aggfunc=_series_first),
+                    greige=pd.NamedAgg(column='GREIGE ITEM', aggfunc=_series_first),
+                    color_num=pd.NamedAgg(column='COLOR NUMBER', aggfunc=_series_first),
+                    wip_width1=pd.NamedAgg(column='WD', aggfunc=lambda x: _series_first(x) * 2),
+                    wip_width2=pd.NamedAgg(column='WD', aggfunc=lambda x: _series_first(x) * 3),
+                    yld=pd.NamedAgg(column='Yield', aggfunc=_series_first),
+                    jets=pd.NamedAgg(column='jets', aggfunc=_series_first)
+                )
+                res = unique.reset_index()
+            case _:
+                raise RuntimeError(f'Unrecognized buildable info {repr(name)}')
 
-    def _get_jets(row):
-        jets = []
-        for i in filter(lambda i: i < 4 or i > 5, range(10)):
-            if not pd.isna(row[f'JET {i+1}']):
-                jets.append(f'Jet-{i+1:02}')
-        return ' '.join(jets)
+        outpath = os.path.join(block['dest']['folder'].data, block['dest']['file'].data)
+        outfile = open(outpath, mode='w+')
+
+        for i in res.index:
+            match name:
+                case 'greige_translation':
+                    items = [res.loc[i, 'inventory'], res.loc[i, 'plan']]
+                case 'greige_styles':
+                    roll_tgt = res.loc[i, 'Target']
+                    if roll_tgt > 400:
+                        port_tgt = roll_tgt / 2
+                    else:
+                        port_tgt = roll_tgt
+                    safety = res.loc[i, 'SafetyTgt']
+                    items = [res.loc[i, 'Greige'], res.loc[i, 'GreigeAlt'], res.loc[i, 'GreigeAlt2'],
+                             f'{roll_tgt:.2f}', f'{port_tgt:.2f}', f'{safety:.2f}']
+                case 'dye_formulae':
+                    items = [str(int(res.loc[i, 'COLOR NUMBER'])), res.loc[i, 'name'],
+                             str(int(res.loc[i, 'shade']))]
+                case 'fab_items':
+                    wd1 = res.loc[i, 'wip_width1']
+                    wd2 = res.loc[i, 'wip_width2']
+                    if int(wd1) == wd1:
+                        wd1 = int(wd1)
+                    if int(wd2) == wd2:
+                        wd2 = int(wd2)
+                    
+                    items = [res.loc[i, 'PLY 1 PART #'], res.loc[i, 'item'], res.loc[i, 'master'],
+                             res.loc[i, 'greige'], str(int(res.loc[i, 'color_num'])), str(wd1), str(wd2),
+                             f'{res.loc[i, 'yld']:.5f}', res.loc[i, 'jets']]
+            
+            outfile.write('\t'.join(items) + '\n')
+        
+        outfile.truncate()
+        outfile.close()
     
-    fab_df['jets'] = fab_df.agg(_get_jets, axis=1)
+    return _load_dat_file('dest', block['dest'])
 
-    fab_df = fab_df.groupby('item').agg(
-        master=pd.NamedAgg(column='STYLE', aggfunc=_series_first),
-        greige=pd.NamedAgg(column='GREIGE ITEM', aggfunc=_series_first),
-        color_num=pd.NamedAgg(column='COLOR NUMBER', aggfunc=_series_first),
-        wip_width1=pd.NamedAgg(column='WD', aggfunc=lambda x: _series_first(x) * 2),
-        wip_width2=pd.NamedAgg(column='WD', aggfunc=lambda x: _series_first(x) * 3),
-        yld=pd.NamedAgg(column='Yield', aggfunc=_series_first),
-        jets=pd.NamedAgg(column='jets', aggfunc=_series_first)
-    )
-    fab_df = fab_df.reset_index()
+def _series_first(srs: pd.Series):
+    return list(srs)[0]
+
+def _load_product_info(name: str, block: dict, rebuild: bool = False):
+    dye_df = _load_block('dye_formulae', block['dye_formulae'], rebuild=rebuild)
+    fab_df = _load_block('fab_items', block['fab_items'], rebuild=rebuild)
+    grg_df = _load_block('greige_styles', block=['greige_styles'], rebuild=rebuild)
+
+    fab_df = fab_df.merge(grg_df, left_on='greige', right_on='style')
 
     return {
-        'dye_formulae': unique_dyes,
+        'dye_formulae': dye_df,
         'fab_items': fab_df,
         'greige_styles': grg_df
     }
 
-def _load_dye_orders(name: str, block: dict):
-    adaptive = _load_block('adaptive', block['adaptive'])
-    pa_714 = _load_block('pa_714', block['pa_714'])
-    fab_items = _load_block('fab_items', block['fab_items'])
-    fab_items = _convert_fab_items(fab_items)
-    fab_items = fab_items[~fab_items['GREIGE ITEM'].str.contains('CAT')]
+def _load_dye_plan(name: str, block: dict, rebuild: bool = False):
+    inv_df = _load_block('avail_greige', block['avail_greige'], rebuild=rebuild)
+    sched_df = _load_block('future_orders', block['future_orders'], rebuild=rebuild)
+    grg_df = _load_block('safety_tgts', block['safety_tgts'], rebuild=rebuild)
 
-    bad_rows = pa_714[pa_714['Sales Rep'] == 'Sales Rep'].index
-    pa_714 = pa_714.drop(bad_rows)
-    for col in ('Line Width', 'Dye Order', 'DO Qty'):
-        pa_714[col] = pa_714[col].astype('float64')
-
-    def convert_dye_order(x):
-        if pd.isna(x):
-            return ''
-        return f'{int(x):010}'
-    pa_714['mo'] = pa_714['Dye Order'].apply(convert_dye_order)
-
-    dye_data = {
-        'job': [], 'dyelot': [], 'item': [], 'machine': [], 'start': [], 'end': []
-    }
-
-    for i in adaptive.index:
-        if pd.isna(adaptive.loc[i, 'StartTime']) or pd.isna(adaptive.loc[i, 'EndTime']):
-            continue
-
-        job_id = adaptive.loc[i, 'JobID']
-        lots = job_id.split('@')[0].split('/')
-        for lot in lots:
-            dye_data['job'].append(job_id)
-            dye_data['dyelot'].append(lot)
-            dye_data['start'].append(adaptive.loc[i, 'StartTime'])
-            dye_data['end'].append(adaptive.loc[i, 'EndTime'])
-            
-            if re.match('[0-9]{10}', lot):
-                item_rows = pa_714[pa_714['mo'] == lot]
-                if len(item_rows) > 0:
-                    first = list(item_rows.index)[0]
-                    stock_item = pa_714.loc[first, 'Item']
-                    comps = stock_item.split('-')
-
-                    try:
-                        color = int(comps[-1])
-                        master = '-'.join(comps[:-1])
-                        width = pa_714.loc[first, 'Line Width']
-
-                        fab_matches2 = fab_items[(fab_items['STYLE'] == master[3:]) & (fab_items['COLOR NUMBER'] == color) \
-                                                & (fab_items['WD'] == width/2)]
-                        fab_matches3 = fab_items[(fab_items['STYLE'] == master[3:]) & (fab_items['COLOR NUMBER'] == color) \
-                                                & (fab_items['WD'] == width/3)]
-                        if len(fab_matches2) == 0 and len(fab_matches3) == 0:
-                            dye_data['item'].append('NONE')
-                        elif len(fab_matches2) > 0:
-                            first = list(fab_matches2.index)[0]
-                            dye_data['item'].append(fab_items.loc[first, 'item'])
-                        else:
-                            first = list(fab_matches3.index)[0]
-                            dye_data['item'].append(fab_items.loc[first, 'item'])
-                    except ValueError:
-                        dye_data['item'].append(stock_item)
-                else:
-                    dye_data['item'].append('NONE')
-            elif 'HEAVY STRIP' in lot:
-                dye_data['item'].append('HEAVYSTRIP')
-            elif 'STRIP' in lot:
-                dye_data['item'].append('STRIP')
-            else:
-                dye_data['item'].append('NONE')
-            dye_data['machine'].append(adaptive.loc[i, 'Machine'])
-    
-    return pd.DataFrame(data=dye_data)
-
-def _load_dye_plan(name: str, block: dict):
-    inv_df = _load_block('avail_greige', block['avail_greige'])
-    sched_df = _load_block('future_orders', block['future_orders'])
-    grg_df = _load_block('safety_tgts', block['safety_tgts'])
-
-    grg_df = grg_df.set_index('GreigeAlt')
+    grg_df = grg_df.set_index('style')
 
     sched_df['week'] = sched_df['start'].apply(lambda d: d.isocalendar().week)
     sched_df['is_new1'] = sched_df['roll1'].str.contains('NEW|PLAN')
     sched_df['is_new2'] = sched_df['roll2'].str.contains('NEW|PLAN')
 
     def map_to_alt_grg(g):
-        return grg_df.loc[g, 'GreigeAlt2']
+        return grg_df.loc[g, 'alt2']
     sched_df['greige2'] = sched_df['greige'].apply(map_to_alt_grg).astype('string')
 
     inv_df['greige2'] = inv_df['greige'].apply(map_to_alt_grg).astype('string')
@@ -357,19 +351,16 @@ def _subtract_business_days(start: dt.datetime, ndays: int):
             ndays = 0
     return res
 
-def _load_lam_data(name: str, block: dict, weekof: dt.datetime):
-    move_times = _load_block('move_times', block['move_times'])
-    lam_items = _load_block('lam_items', block['lam_items'])
-    lam_items = df_cols_as_str(lam_items, ['Stock Item', 'Ply1 Item'])
+def _load_lam_data(name: str, block: dict, weekof: dt.datetime, rebuild: bool = False):
+    move_times = _load_block('move_times', block['move_times'], rebuild=rebuild)
+    lam_items = _load_block('lam_items', block['lam_items'], rebuild=rebuild)
 
     pull_df = _build_move_times(move_times, lam_items)
-    raw_release = _load_block('lam_release', block['lam_release'])
-    raw_release = df_cols_as_str(raw_release, ['Stock Item', 'Ply1 Item'])
+    raw_release = _load_block('lam_release', block['lam_release'], rebuild=rebuild)
 
     rls_cols = ['lam', 'ply1', 'plant', 'hard_date', 'soft_date', 'qty']
     rls_data = { col: [] for col in rls_cols }
 
-    cur_week = weekof
     for key, grp in raw_release.groupby(['Stock Item', 'Plant']):
         lam, plant = key
         first = list(grp.index)[0]
@@ -415,17 +406,18 @@ def _load_lam_data(name: str, block: dict, weekof: dt.datetime):
         'lam_reqs': lam_reqs, 'raw_release': raw_release
     }
 
-def _load_pa_reqs(name: str, block: dict, weekof: dt.datetime):
-    lam_data = _load_block('lam_data', block['lam_data'])
-    pa_xref = _load_block('pa_xref', block['pa_xref'])
-    pa_wip = _load_block('pa_wip', block['pa_wip'])
-    pa_wip_do = _load_block('pa_wip_do', block['pa_wip_do'])
+def _load_pa_reqs(name: str, block: dict, weekof: dt.datetime, rebuild: bool = False):
+    lam_data = _load_block('lam_data', block['lam_data'], rebuild=rebuild)
+    pa_xref = _load_block('pa_xref', block['pa_xref'], rebuild=rebuild)
+    pa_wip = _load_block('pa_wip', block['pa_wip'], rebuild=rebuild)
+    pa_wip_do = _load_block('pa_wip_do', block['pa_wip_do'], rebuild=rebuild)
 
-    cur_week_num = weekof.isocalendar()[1]
+    return {
+        'lam_data': lam_data, 'pa_xref': pa_xref, 'pa_wip': pa_wip,
+        'pa_wip_do': pa_wip_do
+    }
 
-    lam_reqs: pd.DataFrame = lam_data['lam_reqs']
-
-def _load_block(name: str, block: dict):
+def _load_block(name: str, block: dict, rebuild: bool = False):
     _check_required_keys(name, block)
 
     match block['@dtype']:
@@ -435,20 +427,22 @@ def _load_block(name: str, block: dict):
             return _load_csv(name, block)
         case 'JSON':
             return _load_json(name, block)
+        case 'Buildable':
+            return _load_buildable(name, block, rebuild=rebuild)
         case 'ProductInfo':
-            return _load_product_info(name, block)
-        case 'DyeOrders':
-            return _load_dye_orders(name, block)
+            return _load_product_info(name, block, rebuild=rebuild)
         case 'PADyePlan':
-            return _load_dye_plan(name, block)
+            return _load_dye_plan(name, block, rebuild=rebuild)
         case 'PAReqs':
-            return _load_block('lam_data', block['lam_data'])
+            weekof_str = globals()['_INFO']['INPUT_ARGS']['req_weekof'].data
+            year, month, day = int(weekof_str[:4]), int(weekof_str[4:6]), int(weekof_str[-2:])
+            weekof = dt.datetime(year, month, day)
+            return _load_pa_reqs(name, block, weekof, rebuild=rebuild)
         case 'LamData':
             weekof_str = globals()['_INFO']['INPUT_ARGS']['req_weekof'].data
             year, month, day = int(weekof_str[:4]), int(weekof_str[4:6]), int(weekof_str[-2:])
             weekof = dt.datetime(year, month, day)
-            print(weekof)
-            return _load_lam_data(name, block, weekof)
+            return _load_lam_data(name, block, weekof, rebuild=rebuild)
         case _:
             raise RuntimeError('Not working yet')
 
@@ -462,7 +456,7 @@ def init_info(fpath: str, **kwargs):
     for k in res:
         globals()['_INFO'][k] = res[k]
     
-def load_data(name: str):
+def load_data(name: str, rebuild: bool = False):
     if name not in globals()['_INFO']:
         raise KeyError(f'Unrecognized data {repr(name)}')
-    return _load_block(name, globals()['_INFO'][name])
+    return _load_block(name, globals()['_INFO'][name], rebuild=rebuild)
